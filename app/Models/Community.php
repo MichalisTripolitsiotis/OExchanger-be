424c3d4454aeb800730663b2feddf29999efed98
Michalis Tripolitsiotis <michalistripolitsiotis@gmail.com>
2023-03-04 13:52:50 +0200

    Add posts to communities API

--- app/Models/Community.php
+++ app/Models/Community.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
@@ -46,5 +47,13 @@ class Community extends Model
     public function subscribers(): BelongsToMany
     {
         return $this->belongsToMany(User::class, 'subscribed_communities')->withTimestamps();
+    }
+
+    /**
+     * @return HasMany
+     */
+    public function posts(): HasMany
+    {
+        return $this->hasMany(Post::class);
     }
 }
